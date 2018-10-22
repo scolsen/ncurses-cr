2df31aa4fe71bcdc066e42969ee48d94fae4da83
Scott Olsen <scg.olsen@gmail.com>
2018-10-21 23:51:48 -0400

    Use LibC::Ints instead of Int32s.

--- src/ncurses/ncurses.cr
+++ src/ncurses/ncurses.cr
@@ -1,6 +1,6 @@
 @[Link("ncurses")]
 lib LibCurses
-  type Chtype = Int32
+  type Chtype = LibC::Int
   type Window = Void
   type WideChar = LibC::Char # TODO: Redefine this appropriately.
   type WideString = LibC::Char # TODO: Redefine this appropriately. Wrapper for cchar_t, a string of wide characters.
@@ -11,114 +11,114 @@ lib LibCurses
   # Complex character functions.
   # wch is placed in the window at the current position and then advanced.
   # see: http://invisible-island.net/ncurses/man/curs_add_wch.3x.htmlhttp://invisible-island.net/ncurses/man/curs_add_wch.3x.html
-  fun add_wch(wch : WideChar*) : Int32
-  fun wadd_wch(win : Window*, wch : WideChar*) : Int32
-  fun mvadd_wch(y : Int32, x : Int32, wch : WideChar*) : Int32
-  fun mvwadd_wch(win : Window*, y : Int32, x : Int32, wch : WideChar*) : Int32
-  fun echo_wchar (wch : WideChar*) : Int32
-  fun wecho_wchar (win : Window*, wch : WideChar*) : Int32
+  fun add_wch(wch : WideChar*) : LibC::Int
+  fun wadd_wch(win : Window*, wch : WideChar*) : LibC::Int
+  fun mvadd_wch(y : LibC::Int, x : LibC::Int, wch : WideChar*) : LibC::Int
+  fun mvwadd_wch(win : Window*, y : LibC::Int, x : LibC::Int, wch : WideChar*) : LibC::Int
+  fun echo_wchar (wch : WideChar*) : LibC::Int
+  fun wecho_wchar (win : Window*, wch : WideChar*) : LibC::Int
 
   # Complex character string functions.
   # Copy an null-terminated array of complex characters into a window at the current cursor position. Arguments with an n copy at most n elements. 
   # see: http://invisible-island.net/ncurses/man/curs_add_wchstr.3x.html
-  fun add_wchstr(wchstr : WideChar*) : Int32
-  fun add_wchnstr(wchstr : WideChar*, n : Int32) : Int32
-  fun wadd_wchstr(win : Window*, wchstr : WideChar*) : Int32
-  fun wadd_wchnstr(win : Window*, wchstr : WideChar*, n : Int32) : Int32
-  fun mvadd_wchstr(y : Int32, x : Int32, wchstr : WideChar*) : Int32
-  fun mvadd_wchnstr(y : Int32, x : Int32, wchstr : WideChar*, n : Int32) : Int32
-  fun mvwadd_wchstr(win : Window*, y : Int32, x : Int32, wchstr : WideChar*) : Int32
-  fun mvwadd_wchnstr(win : Window*, y : Int32, x : Int32, wchstr : WideChar*, n : Int32) : Int32
+  fun add_wchstr(wchstr : WideChar*) : LibC::Int
+  fun add_wchnstr(wchstr : WideChar*, n : LibC::Int) : LibC::Int
+  fun wadd_wchstr(win : Window*, wchstr : WideChar*) : LibC::Int
+  fun wadd_wchnstr(win : Window*, wchstr : WideChar*, n : LibC::Int) : LibC::Int
+  fun mvadd_wchstr(y : LibC::Int, x : LibC::Int, wchstr : WideChar*) : LibC::Int
+  fun mvadd_wchnstr(y : LibC::Int, x : LibC::Int, wchstr : WideChar*, n : LibC::Int) : LibC::Int
+  fun mvwadd_wchstr(win : Window*, y : LibC::Int, x : LibC::Int, wchstr : WideChar*) : LibC::Int
+  fun mvwadd_wchnstr(win : Window*, y : LibC::Int, x : LibC::Int, wchstr : WideChar*, n : LibC::Int) : LibC::Int
 
   # Character functions.
   # see: http://invisible-island.net/ncurses/man/curs_addch.3x.html
-  fun addch(ch : Chtype) : Int32 
-  fun waddch(win : Window*, ch : Chtype) : Int32
-  fun mvaddch(y : Int32, x : Int32, ch : Chtype) : Int32
-  fun mvwaddch(win : Window*, y : Int32, x : Int32, ch : Chtype) : Int32
-  fun echochar(ch : Chtype) : Int32
-  fun wechochar(win : Window*, ch : Chtype) : Int32
+  fun addch(ch : Chtype) : LibC::Int 
+  fun waddch(win : Window*, ch : Chtype) : LibC::Int
+  fun mvaddch(y : LibC::Int, x : LibC::Int, ch : Chtype) : LibC::Int
+  fun mvwaddch(win : Window*, y : LibC::Int, x : LibC::Int, ch : Chtype) : LibC::Int
+  fun echochar(ch : Chtype) : LibC::Int
+  fun wechochar(win : Window*, ch : Chtype) : LibC::Int
 
   # Character string functions.
   # see: http://invisible-island.net/ncurses/man/curs_addchstr.3x.html 
-  fun addchstr(chstr : Chtype*) : Int32
-  fun addchnstr(chstr : Chtype*, n : Int32) : Int32
-  fun waddchstr(win : Window*, chstr : Chtype*) : Int32
-  fun waddchnstr(win : Window*, chstr : Chtype*, n : Int32) : Int32
-  fun mvaddchstr(y : Int32, x : Int32, chstr : Chtype*) : Int32
-  fun mvaddchnstr(y : Int32, x : Int32, chstr : Chtype*, n : Int32) : Int32
-  fun mvwaddchstr(win : Window*, y : Int32, x : Int32, chstr : Chtype*) : Int32
-  fun mvwaddchnstr(win : Window*, y : Int32, x : Int32, chstr : Chtype*, n : Int32) : Int32
+  fun addchstr(chstr : Chtype*) : LibC::Int
+  fun addchnstr(chstr : Chtype*, n : LibC::Int) : LibC::Int
+  fun waddchstr(win : Window*, chstr : Chtype*) : LibC::Int
+  fun waddchnstr(win : Window*, chstr : Chtype*, n : LibC::Int) : LibC::Int
+  fun mvaddchstr(y : LibC::Int, x : LibC::Int, chstr : Chtype*) : LibC::Int
+  fun mvaddchnstr(y : LibC::Int, x : LibC::Int, chstr : Chtype*, n : LibC::Int) : LibC::Int
+  fun mvwaddchstr(win : Window*, y : LibC::Int, x : LibC::Int, chstr : Chtype*) : LibC::Int
+  fun mvwaddchnstr(win : Window*, y : LibC::Int, x : LibC::Int, chstr : Chtype*, n : LibC::Int) : LibC::Int
 
   # String functions.
   # see: http://invisible-island.net/ncurses/man/curs_addstr.3x.html 
-  fun addstr(str : LibC::Char*) : Int32
-  fun addnstr(str : LibC::Char*, n : Int32) : Int32
-  fun waddstr(win : Window*, str : LibC::Char*) : Int32
-  fun waddnstr(win : Window*, str : LibC::Char*, n : Int32) : Int32
-  fun mvaddstr(y : Int32, x : Int32, str : LibC::Char*) : Int32
-  fun mvaddnstr(y : Int32, x : Int32, str : LibC::Char*, n : Int32) : Int32
-  fun mvwaddstr(win : Window*, y : Int32, x : Int32, str : LibC::Char*) : Int32
-  fun mvwaddnstr(win : Window*, y : Int32, x : Int32, str : LibC::Char*, n : Int32) : Int32
+  fun addstr(str : LibC::Char*) : LibC::Int
+  fun addnstr(str : LibC::Char*, n : LibC::Int) : LibC::Int
+  fun waddstr(win : Window*, str : LibC::Char*) : LibC::Int
+  fun waddnstr(win : Window*, str : LibC::Char*, n : LibC::Int) : LibC::Int
+  fun mvaddstr(y : LibC::Int, x : LibC::Int, str : LibC::Char*) : LibC::Int
+  fun mvaddnstr(y : LibC::Int, x : LibC::Int, str : LibC::Char*, n : LibC::Int) : LibC::Int
+  fun mvwaddstr(win : Window*, y : LibC::Int, x : LibC::Int, str : LibC::Char*) : LibC::Int
+  fun mvwaddnstr(win : Window*, y : LibC::Int, x : LibC::Int, str : LibC::Char*, n : LibC::Int) : LibC::Int
 
   # Wide string functions.
   # see: http://invisible-island.net/ncurses/man/curs_addwstr.3x.html
-  fun addwstr() : Int32
-  fun addnwstr() : Int32
-  fun waddwstr() : Int32
-  fun waddnwstr() : Int32
-  fun mvaddwstr() : Int32
-  fun mvaddnwstr() : Int32
-  fun mvwaddwstr() : Int32
-  fun mvwaddnwstr() : Int32
+  fun addwstr() : LibC::Int
+  fun addnwstr() : LibC::Int
+  fun waddwstr() : LibC::Int
+  fun waddnwstr() : LibC::Int
+  fun mvaddwstr() : LibC::Int
+  fun mvaddnwstr() : LibC::Int
+  fun mvwaddwstr() : LibC::Int
+  fun mvwaddnwstr() : LibC::Int
 
   # Color pair functions.
   # see: http://invisible-island.net/ncurses/man/new_pair.3x.html
-  fun alloc_pair(fg : Int32, bg : Int32) : Int32
-  fun find_pair(fg : Int32, bg : Int32) : Int32
-  fun free_pair(pair : Int32) : Int32
+  fun alloc_pair(fg : LibC::Int, bg : LibC::Int) : LibC::Int
+  fun find_pair(fg : LibC::Int, bg : LibC::Int) : LibC::Int
+  fun free_pair(pair : LibC::Int) : LibC::Int
 
   # Default color functions.
   # see: http://invisible-island.net/ncurses/man/default_colors.3x.html
-  fun use_default_colors : Int32
-  fun assume_default_colors(fg : Int32, bg : Int32) : Int32
+  fun use_default_colors : LibC::Int
+  fun assume_default_colors(fg : LibC::Int, bg : LibC::Int) : LibC::Int
 
   # Attribute functions.
   # see: http://invisible-island.net/ncurses/man/curs_attr.3x.html
-  fun attr_get(attrs : Attr, pair : Int16*, opts : Void*) : Int32
-  fun wattr_get(win : Window*, attrs : Attr, pair : Int16*, opts : Void*) : Int32
-  fun attr_set(attrs : Attr, pair : Int16, opts : Void*) : Int32
-  fun wattr_set(win : Window*, attrs : Attr, pair : Int16, opts : Void*) : Int32
-  fun attr_off(attrs : Attr, opts : Void*) : Int32
-  fun wattr_off(win : Window*, attrs : Attr, opts : Void*) : Int32
-  fun attr_on(attrs : Attr, opts : Void*) : Int32
-  fun wattr_on(win : Window*, attrs: Attr, opts : Void*) : Int32
-  fun attroff(attrs : Int32) : Int32
-  fun wattroff(win : Window*, attrs : Int32) : Int32
-  fun attron(attrs : Int32) : Int32
-  fun wattron(win : Window*, attrs : Int32) : Int32
-  fun attrset(attrs : Int32) : Int32
-  fun wattrset(win : Window*, attrs : Int32) : Int32
-  fun chgat(n : Int32, attr : Attr, pair : Int16, opts : Void*) : Int32
-  fun wchgat(win : Window*, n : Int32, attr : Attr, pair : Int16, opts : Void*) : Int32
-  fun mvchgat(y : Int32, x : Int32, n : Int32, attr : Attr, pair : Int16, opts : Void*) : Int32
-  fun mvwchgat(win : Window*, y : Int32, x : Int32, n : Int32, attr : Attr, pair : Int16, opts : Void*) : Int32
-  fun color_set(pair : Int16, opts : Void*) : Int32
-  fun wcolor_set(win : Window*, pair : Int16, opts : Void*) : Int32
-  fun standend : Int32
-  fun wstandend(win : Window*) : Int32
-  fun standout : Int32
-  fun wstandout(win : Window*) : Int32
+  fun attr_get(attrs : Attr, pair : Int16*, opts : Void*) : LibC::Int
+  fun wattr_get(win : Window*, attrs : Attr, pair : Int16*, opts : Void*) : LibC::Int
+  fun attr_set(attrs : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun wattr_set(win : Window*, attrs : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun attr_off(attrs : Attr, opts : Void*) : LibC::Int
+  fun wattr_off(win : Window*, attrs : Attr, opts : Void*) : LibC::Int
+  fun attr_on(attrs : Attr, opts : Void*) : LibC::Int
+  fun wattr_on(win : Window*, attrs: Attr, opts : Void*) : LibC::Int
+  fun attroff(attrs : LibC::Int) : LibC::Int
+  fun wattroff(win : Window*, attrs : LibC::Int) : LibC::Int
+  fun attron(attrs : LibC::Int) : LibC::Int
+  fun wattron(win : Window*, attrs : LibC::Int) : LibC::Int
+  fun attrset(attrs : LibC::Int) : LibC::Int
+  fun wattrset(win : Window*, attrs : LibC::Int) : LibC::Int
+  fun chgat(n : LibC::Int, attr : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun wchgat(win : Window*, n : LibC::Int, attr : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun mvchgat(y : LibC::Int, x : LibC::Int, n : LibC::Int, attr : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun mvwchgat(win : Window*, y : LibC::Int, x : LibC::Int, n : LibC::Int, attr : Attr, pair : Int16, opts : Void*) : LibC::Int
+  fun color_set(pair : Int16, opts : Void*) : LibC::Int
+  fun wcolor_set(win : Window*, pair : Int16, opts : Void*) : LibC::Int
+  fun standend : LibC::Int
+  fun wstandend(win : Window*) : LibC::Int
+  fun standout : LibC::Int
+  fun wstandout(win : Window*) : LibC::Int
 
   # Terminal properties functions.
   # see: http://invisible-island.net/ncurses/man/curs_termattrs.3x.html
-  fun baudrate : Int32
+  fun baudrate : LibC::Int
   fun erasechar : LibC::Char
-  fun erasewchar(ch : WideChar*) : Int32
+  fun erasewchar(ch : WideChar*) : LibC::Int
   fun has_ic : Bool
   fun has_il : Bool
   fun killchar : LibC::Char
-  fun killwchar(ch : WideChar*) : Int32
+  fun killwchar(ch : WideChar*) : LibC::Int
   fun longname : LibC::Char*
   fun term_attrs : Attr
   fun termattrs : Chtype
@@ -126,8 +126,8 @@ lib LibCurses
 
   # Terminal alert functions.
   # see: http://invisible-island.net/ncurses/man/curs_beep.3x.html
-  fun beep : Int32
-  fun flash : Int32
+  fun beep : LibC::Int
+  fun flash : LibC::Int
  
   # Background functions.
   # see: http://invisible-island.net/ncurses/man/curs_bkgd.3x.html
