609d0db230872d620bd0595f06103276206851ea
Scott Olsen <scg.olsen@gmail.com>
2018-10-23 00:15:08 -0400

    Add Cantrip module. Wrap Curses.

--- src/ncurses.cr
+++ src/ncurses.cr
@@ -1,6 +1,45 @@
 require "./ncurses/*"
 
 # TODO: Write documentation for `Ncurses`
-module Ncurses
-  # TODO: Put your code here
+module Cantrip
+  extend self  
+  
+  # Start cantrip.
+  # Wraps `LibCurses.initscr` functions to enter curses mode.
+  # Sets a number of module class variables to manage overall state.
+  # @@main_window stores the main window reference returned by LibCurses.  
+  def start 
+    raise "Cantrip is already initalized" if @@started
+    Window.new(LibCurses.initscr)
+    @@started = true 
+  end
+
+  # Stops cantrip.
+  # Wrapper around `LibCurses.endwin`.
+  def stop
+    LibCurses.endwin
+  end
+
+  # Enter character input mode. 
+  # Convenience function to invoke the common sequence of `LibCurses.cbreak`
+  # and `LibCurses.noecho`
+  def character_mode
+    LibCurses.cbreak
+    LibCurses.noecho
+  end
+
+  class Window
+    
+    def initialize(window : LibCurses::Window*)
+      @window = window
+    end
+  
+    def initialize(lines : Int32, columns : Int32, y : Int32, x : Int32)
+      @window = LibCurses.newwin(lines, columns, y, x)
+    end
+
+    def to_unsafe
+      @window
+    end
+  end
 end
