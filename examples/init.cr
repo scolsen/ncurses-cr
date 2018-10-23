54bd153920457c853c78e57a7f0bef9444296aa6
Scott Olsen <scg.olsen@gmail.com>
2018-10-23 00:15:33 -0400

    Update example to use Cantrip.

--- examples/init.cr
+++ examples/init.cr
@@ -2,11 +2,20 @@ require "../src/ncurses"
 
 x = 123
 
-LibCurses.initscr
+#LibCurses.initscr
+#LibCurses.cbreak
+#LibCurses.addch x
+#y = LibCurses.getch
+#while y != 113
+#y = LibCurses.getch  
+#end
+#LibCurses.endwin
+
+Cantrip.start
 LibCurses.cbreak
 LibCurses.addch x
 y = LibCurses.getch
 while y != 113
-y = LibCurses.getch  
+  y = LibCurses.getch  
 end
-LibCurses.endwin
+Cantrip.stop
